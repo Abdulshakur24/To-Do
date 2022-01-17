fd5cb2692615faee82f0a8f7c394c015ac15a3b5
abdulshakur <abdulshakur486@gmail.com>
2022-01-17 17:55:32 +0300

    improved breaking points view ports.

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,30 +1,17 @@
 export const theme = {
-  // Temp fonts
-  fonts: {
-    title: "Space Grotesk, sans-serif",
-    main: "Space Grotesk, sans-serif",
-  },
-  // Colors for layout
-  colors: {
-    primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
-    accent1: "hsl(34.9,98.6%,72.9%)",
-    button: "hsl(205.1,100%,36.1%)",
-    background2: "hsl(232.7,27.3%,23.7%)",
-  },
   // Breakpoints for responsive design
   breakpoints: {
     minWidth: {
-      sm: "screen and (min-width: 375px)",
-      md: "screen and (min-width: 768px)",
-      lg: "screen and (min-width: 1024px)",
-      xl: "screen and (min-width: 1280px)",
+      sm: "screen and (min-width: 23.4375em)",
+      md: "screen and (min-width: 40em)",
+      lg: "screen and (min-width: 64em)",
+      xl: "screen and (min-width: 90em)",
     },
     maxWidth: {
-      sm: "screen and (max-width: 375px)",
-      md: "screen and (max-width: 768px)",
-      lg: "screen and (max-width: 1024px)",
-      xl: "screen and (max-width: 1280px)",
+      sm: "screen and (max-width: 23.375rem)",
+      md: "screen and (max-width: 39.9375em)",
+      lg: "screen and (max-width: 63.9375em)",
+      xl: "screen and (max-width: 89.9375em)",
     },
   },
 };
